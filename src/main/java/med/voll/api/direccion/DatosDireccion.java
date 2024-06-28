7ec708a48a1de5c40226ddf0e73e9cd052f72fe5
Pablo Giress <pablogiress@gmail.com>
2024-06-27 21:46:40 -0500

    initial

---
+++ src/main/java/med/voll/api/direccion/DatosDireccion.java
@@ -0,0 +1,7 @@
+package med.voll.api.direccion;
+
+public record DatosDireccion(String calle, String distrito, String ciudad, String numero,
+                             String complemento) {
+
+
+}
---
+++ src/main/resources/db/migration/V1__create-table-medicos.sql
@@ -0,0 +1,13 @@
+create table medicos(
+    id bigint not null auto_increment,
+    nombre varchar(100) not null,
+    email varchar(100) not null unique,
+    documento varchar(15) not null unique,
+    especialidad varchar(100) not null,
+    calle varchar(100) not null,
+    distrito varchar(100) not null,
+    complemento varchar(100),
+    numero varchar(20),
+    ciudad varchar(100) not null,
+    primary key(id)
+);
