a2b13d058329c85971fce24a2f808c9e64298e41
Pablo Giress <pablogiress@gmail.com>
2024-07-11 20:30:12 -0500

    Se agregan mas funciones

---
+++ src/main/java/med/voll/api/domain/usuarios/UsuarioRepository.java
@@ -0,0 +1,6 @@
+package med.voll.api.domain.usuarios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
